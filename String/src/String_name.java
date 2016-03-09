de82ef467cd0dcecce93a189960acd170e0774a7
Karan <Karan@Karan-PC>
2016-03-09 20:24:34 +0545

    Initial checkin

---
+++ String/src/String_name.java
@@ -0,0 +1,15 @@
+
+public class String_name {
+
+	
+	public static void main(String[] args) {
+		String name="Raju";
+		String name1=" Karan ";
+		System.out.println(name);
+		System.out.println(name1);
+		System.out.println(name1.trim());
+		
+
+	}
+
+}
