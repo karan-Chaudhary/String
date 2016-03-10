bb7ef883a6b0d15864f457271f22b4f887216227
Karan <Karan@Karan-PC>
2016-03-10 10:47:20 +0545

    huhuggy

---
+++ Hello Wolrd/src/Hello_World.java
@@ -0,0 +1,23 @@
+
+public class Hello_World {
+
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello world");
+		
+		Nepal visa=new Nepal();
+		visa.getDisplay();
+	}
+
+}
+class Nepal{
+	
+	public void getDisplay(){
+		System.out.println("Hello Nepal");
+		
+	}
+	
+	
+}
